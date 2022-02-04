6f3c2011d967525620b0ff0993db0424a1c6e46e
Kirsty Chatterton <kirsty.chatterton@pwc.com>
2022-02-04 10:01:57 +0000

    Initial commit

---
+++ PDFGenerator/Utility/TemplateGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace PDF_Generator.Utility
+{
+    public static class TemplateGenerator
+    {
+        public static string GetHTMLString(string data)
+        {
+            var sb = new StringBuilder();
+            sb.Append(@"
+                        <html>
+                            <head>
+                            </head>
+                            <body>
+                                <div class='header'><h1>This is the generated PDF report!!!</h1></div>
+                                <h1>"+ data + "</h1></body>");
+            return sb.ToString();
+        }
+    }
+}
